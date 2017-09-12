ac634999e68d671ceb8e6900d98a4b8a33cd9dfc
Chalamphong Pandey <2201pandey@gmail.com>
2017-09-13 00:54:52 +0700

    Add basic functionality

---
+++ src/index.js
@@ -0,0 +1,95 @@
+import { indexOf, values, isNumber } from 'lodash';
+
+/**
+ * The main class for Satang
+ */
+
+export const Currencies = {
+  THB: 'THB',
+  USD: 'USD'
+};
+
+const CurrencyData = {};
+
+CurrencyData[Currencies.THB] = {
+  symbol: '฿',
+  code: 'thb',
+  prettyCode: 'THB',
+  fullName: 'Thai Baht',
+  locale: 'th-TH'
+};
+
+CurrencyData[Currencies.USD] = {
+  symbol: '$',
+  code: 'usd',
+  prettyCode: 'USD',
+  fullName: 'United States Dollar',
+  locale: 'en-US'
+};
+
+export {
+  CurrencyData
+};
+
+class Satang {
+  /**
+   * constructor for the Satang class
+   * @param  {String} baseCurrency  has to be one of the options available in Currencies. Defaults to THB
+   * @param  {Number} money         the amount of money this instance of Satang represents in subunits. Money in satang for Thai baht or in cents for Dollar
+   */
+  constructor(baseCurrency=Currencies.THB, money=0) {
+    const supportedCurrencies = values(Currencies);
+    if (indexOf(supportedCurrencies, baseCurrency) < 0) {
+      throw new Error(`Unrecognized currency: ${baseCurrency}. Only ${supportedCurrencies.join(',')} are supported`);
+    }
+
+    if (!isNumber(money) || isNaN(money)) {
+      throw new Error('Money is not a number');
+    }
+
+    if (money === Infinity) {
+      throw new Error('Get out');
+    }
+
+    if (money < 0) {
+      throw new Error('Minimum money value is 0');
+    }
+
+    this.baseCurrency = CurrencyData[baseCurrency];
+    this.money = money;
+
+  }
+
+  /**
+   * Returns a pretty string that represents the information stored in this Satang class
+   * @return {String}
+   */
+  display() {
+    const moneyInSubunit = this.money;
+    const miuString =  (Math.round(moneyInSubunit) / 100).toLocaleString(this.baseCurrency.locale, { minimumFractionDigits: 2 });
+
+    return `${this.baseCurrency.symbol}${miuString}`;
+  }
+
+  /**
+   * Merges the supplied satang to this. Can be chained.
+   * @param {Satang} satangToAdd The satang to be added to this
+   *
+   * @return {Satang} return the modified version of this
+   */
+  add(satangToAdd) {
+    if (!(satangToAdd instanceof Satang)) {
+      throw new Error('Only a satang can be added to a satang');
+    }
+
+    if (this.baseCurrency.code !== satangToAdd.baseCurrency.code) {
+      throw new Error('Satangs should be setup with same base currencies if they are to be added');
+    }
+
+    this.money = this.money + satangToAdd.money;
+
+    return this;
+  }
+}
+
+export default Satang;
---
+++ test/satang.test.js
@@ -0,0 +1,60 @@
+import { values } from 'lodash';
+import Satang, { Currencies, CurrencyData } from '../src';
+
+test('it throws an error when initialized with unsupported currency', () => {
+  const baseCurrency = 'thb';
+  expect(() => new Satang('thb', 1000))
+  .toThrowError(`Unrecognized currency: ${baseCurrency}. Only ${values(Currencies).join(',')} are supported`);
+});
+
+test('it throws an error is money is not a number', () => {
+  expect(() => new Satang(Currencies.THB, '100')).toThrowError('Money is not a number');
+  expect(() => new Satang(Currencies.THB, NaN)).toThrowError('Money is not a number');
+  expect(() => new Satang(Currencies.THB, Infinity)).toThrowError('Get out');
+});
+
+test('it throws an error when initialized with the money under 0', () => {
+  expect(() => new Satang(Currencies.THB, -10)).toThrowError('Minimum money value is 0');
+});
+
+test('it sets information correctly', () => {
+  const productPriceInThb = new Satang(Currencies.THB, 100000);
+  const productPriceInUsd = new Satang(Currencies.USD, 100000);
+
+  expect(productPriceInThb.money).toBe(100000);
+  expect(productPriceInThb.baseCurrency).toBe(CurrencyData[Currencies.THB]);
+
+  expect(productPriceInUsd.money).toBe(100000);
+  expect(productPriceInUsd.baseCurrency).toBe(CurrencyData[Currencies.USD]);
+});
+
+test('it displays satang information properly', () => {
+  const productPrice = new Satang(Currencies.THB, 100020);
+  const productPriceRepresentation = productPrice.display();
+  expect(typeof productPriceRepresentation).toBe('string');
+  expect(productPriceRepresentation).toBe('฿1,000.20');
+});
+
+test('it throws an error when adding an object that\'s not a Satang', () => {
+  const productOnePrice = new Satang(Currencies.THB, 100020);
+  const productTwoPrice = new Satang(Currencies.THB, 140020);
+
+  expect(() => productOnePrice.add('productTwoPrice')).toThrowError('Only a satang can be added to a satang');
+});
+
+test('it throws an error when adding a Satang with different base currencies', () => {
+  const productOnePrice = new Satang(Currencies.THB, 100020);
+  const productTwoPrice = new Satang(Currencies.USD, 140020);
+
+  expect(() => productOnePrice.add(productTwoPrice)).toThrowError('Satangs should be setup with same base currencies if they are to be added');
+});
+
+test('it can be manipulated by adding a satang', () => {
+  const productOnePrice = new Satang(Currencies.THB, 100020);
+  const productTwoPrice = new Satang(Currencies.THB, 140020);
+  const cartPrice = new Satang(Currencies.THB, 0);
+
+  cartPrice.add(productOnePrice).add(productTwoPrice);
+  console.log(cartPrice.display());
+  expect(cartPrice.money).toBe(240040);
+});
